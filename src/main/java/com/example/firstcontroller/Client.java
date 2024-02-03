7e1261a61ee9a6fed9b33c4b0f3e26d2a693b5e3
chagaichaimi <chagaichaimi.com>
2024-02-03 21:01:28 +0200

    Reorganization

--- src/main/java/com/example/firstcontroller/Client.java
+++ src/main/java/com/example/firstcontroller/Client.java
@@ -1,46 +1,31 @@
 package com.example.firstcontroller;
 
 public class Client {
+        private String name , phone , email;
+        public Client(String name,String phone ,String email) {
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getPhone() {
+            return phone;
+        }
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+        public String getEmail() {
+            return email;
+        }
 
-    private String userName, password;
-    private int id=0;
-
-    public Client(String name, String pass) {
-        this.userName = name;
-        this.password = pass;
-    }
-    public Client(String name, String pass,int id) {
-        this.userName = name;
-        this.password = pass;
-        this.id =id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
+        public void setEmail(String email) {
+            this.email = email;
+        }
     }
 
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isSameUser(String userName, String password) {
-        return this.userName.equals(userName) && this.password.equals(password);
-    }
-
-}
---
+++ src/main/java/com/example/firstcontroller/ClientsController.java
@@ -0,0 +1,62 @@
+package com.example.firstcontroller;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RestController
+    public class ClientsController {
+        private List<Client> clients = new ArrayList<>();
+
+        @PostConstruct
+        public void addClients(){
+            clients.add(new Client("Hagai Haimi","0527676799", "chagai@ gmail.com"));
+            clients.add(new Client("David Didi","052767679", "david"));
+            clients.add(new Client("Mishel Cohen","0537485678", "mishel78"));
+            clients.add(new Client("Michel Levi","05276767", "michel@gmail.com"));
+        }
+
+        @RequestMapping(value = "get-all-clients", method = RequestMethod.GET)
+        public List<Client> getAllClients() {
+            return clients;
+        }
+
+
+        @RequestMapping(value = "send", method = RequestMethod.POST)
+        public List<Client> send(boolean onlyValidEmail, boolean onlyValidPhone) {
+            List<Client> returnList = new ArrayList<>();
+            for (Client client : this.clients) {
+                if (onlyValidPhone && !onlyValidEmail) {
+                    if (client.getPhone().length() > 9 && client.getPhone().charAt(0) == '0'){
+                        returnList.add(client);
+                        System.out.println("Hallo "+client.getName());
+                    }
+                }
+                if (onlyValidEmail && !onlyValidPhone) {
+                    if (client.getEmail().contains("@")) {
+                        returnList.add(client);
+                        System.out.println("Hallo "+client.getName());
+                    }
+                }
+                if (onlyValidEmail && onlyValidPhone) {
+                    if (client.getEmail().contains("@") && client.getPhone().length() > 9 && client.getPhone().charAt(0) == '0'){
+                        returnList.add(client);
+                        System.out.println("Hallo "+client.getName());
+                    }
+                }
+            }
+            if (returnList.isEmpty()) returnList = clients;
+            return returnList;
+        }
+    }
+
+
+
+
+
